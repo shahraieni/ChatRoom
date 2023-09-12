6b2493f0674900a77bd939a00c67d1a3d54f1495
shahraieni <shahraieni.mehdi@gmail.com>
2023-09-12 22:34:34 +0330

    connect to database

--- API/Entitis/Users.cs
+++ API/Entitis/Users.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entitis
 {
     public class Users
     {
-        
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,10 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            //connect to database
+             services.AddDbContext<DataContext>(options=>{
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
