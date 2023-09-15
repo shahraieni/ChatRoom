9919bfb87c5d4ebf5454e1283cb25ce1a358f05c
shahraieni <shahraieni.mehdi@gmail.com>
2023-09-15 19:43:25 +0330

    Add register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,7 +2,9 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entitis;
+using API.models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,22 +19,26 @@ namespace API.Controllers
             _context = context;
         }
 
-
-
         [HttpPost("register")]
-        public async Task<ActionResult<Users>> Register(string userName , string password)
+        public async Task<ActionResult<Users>> Register(RegisterDto model)
         {
+            if(await IsExistUserName(model.userName))
+            return BadRequest("نام کاربری موجود میباشد");
             using var hmac = new  HMACSHA512();
             var user =new Users{
-                UserName = userName,
-                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+                UserName = model.userName,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(model.password)),
                 PasswordSalt =hmac.Key
             };
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return user;
+        }
 
-            
+        private async Task<bool> IsExistUserName(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+
         }
 
       
