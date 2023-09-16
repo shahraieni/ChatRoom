0d91456291456dd2039338e3840dde3f065aafaf
shahraieni <shahraieni.mehdi@gmail.com>
2023-09-16 16:39:24 +0330

    Add login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using API.Entitis;
 using API.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace API.Controllers
 {
@@ -27,19 +28,34 @@ namespace API.Controllers
             using var hmac = new  HMACSHA512();
             var user =new Users{
                 UserName = model.userName,
+                PasswordSalt =hmac.Key,
                 PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(model.password)),
-                PasswordSalt =hmac.Key
             };
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<Users>> Login([FromBody]LoginDto model)
+        {
+            var user =  await _context.Users.SingleOrDefaultAsync(x => x.UserName.ToLower() == model.userName.ToLower());
+            if(user == null) return BadRequest("نام کاربری یافت نشد");
+                using var hmac = new HMACSHA512(user.PasswordSalt);
+                var  computehash =  hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(model.password));
+                for(int i=0 ; i < computehash.Length; i++)
+                {
+                        if(computehash[i] != user.PasswordHash[i]) return BadRequest(" رمز عبور اشتباه است");
+                }
+                return user;
+
+        }
         private async Task<bool> IsExistUserName(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
 
         }
+
 
       
     }
