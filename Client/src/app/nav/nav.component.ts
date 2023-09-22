973ace9bc1d986d679f8196c683a74b3735ef61d
shahraieni <shahraieni.mehdi@gmail.com>
2023-09-22 13:28:37 +0330

    genert navbar

---
+++ Client/src/app/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent  implements OnInit{
+
+  constructor(){}
+
+  form = new FormGroup({
+    userName : new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    password :new FormControl('',[Validators.required , Validators.minLength(8),Validators.maxLength(15)])
+  })
+  onSubmit(){
+console.log(this.form.value)
+  }
+  ngOnInit(): void {
+
+  }
+
+}
