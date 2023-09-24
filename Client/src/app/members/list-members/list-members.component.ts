cdee28001f141d8cf1812cf75f1238d0c6d1de7b
shahraieni <shahraieni.mehdi@gmail.com>
2023-09-24 11:58:21 +0330

    Add component list ember

---
+++ Client/src/app/members/list-members/list-members.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-members',
+  templateUrl: './list-members.component.html',
+  styleUrls: ['./list-members.component.css']
+})
+export class ListMembersComponent {
+
+}
--- Client/src/app/members/members.module.ts
+++ Client/src/app/members/members.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeMembersComponent } from './home-members/home-members.component';
 import { DitaleMembersComponent } from './ditale-members/ditale-members.component';
+import { ListMembersComponent } from './list-members/list-members.component';
 
 
 
 @NgModule({
   declarations: [
     HomeMembersComponent,
-    DitaleMembersComponent
+    DitaleMembersComponent,
+    ListMembersComponent
   ],
   imports: [
     CommonModule
