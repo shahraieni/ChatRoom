53d1bb1b061d50b7160403f5da0ba665cd6bc18a
shahraieni <shahraieni.mehdi@gmail.com>
2023-09-24 12:06:10 +0330

    Add module list

--- Client/src/app/members/members.module.ts
+++ Client/src/app/members/members.module.ts
@@ -1,11 +1,14 @@
+import { DitaleMembersComponent } from './ditale-members/ditale-members.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MembersRoutingModule } from './members-routing.module';
+import { HomeComponent } from '../home/home.component';
+import { ListMembersComponent } from './list-members/list-members.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent,ListMembersComponent,DitaleMembersComponent],
   imports: [
     CommonModule,
     MembersRoutingModule
