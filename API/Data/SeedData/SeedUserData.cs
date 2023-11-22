025a3d8afee72169246cceb5bba2c0ab35aab2d1
shahraieni <shahraieni.mehdi@gmail.com>
2023-11-22 14:41:30 +0330

    Add seeduserdata

---
+++ API/Data/SeedData/SeedUserData.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Entitis;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data.SeedData
+{
+    public class SeedUserData
+    {
+        public static async Task SeetUser(DataContext context){
+
+            if( !await context.Users.AnyAsync())
+            {
+                var userData= await File.ReadAllTextAsync("Data/SeedData/UserSeedData.json");
+
+                var users = JsonSerializer.Deserialize<List<Users>>(userData);
+                if(users == null) return;
+                foreach (var user in users)
+                {
+                    using  var hmac = new HMACSHA512();
+                    user.UserName = user.UserName.ToLower();
+                    user.PasswordSalt = hmac.Key;
+                    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("P@$$w0rd"));
+                }
+                    await context.Users.AddRangeAsync(users);
+                    await context.SaveChangesAsync();
+            }
+        }
+    }
+}
