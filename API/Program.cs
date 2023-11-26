68cb8d260c38f33918c2167adecf72ef5e53b620
shahraieni <shahraieni.mehdi@gmail.com>
2023-11-26 21:43:38 +0330

    d

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +14,11 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            
+           var host =   CreateHostBuilder(args).Build();
+              using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
