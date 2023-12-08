0d9f4b32c6a3c0d85cf42e1e32d8f36122529700
shahraieni <shahraieni.mehdi@gmail.com>
2023-12-08 20:37:30 +0330

    detals member

--- Client/src/app/members/ditale-members/ditale-members.component.ts
+++ Client/src/app/members/ditale-members/ditale-members.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ditale-members',
   templateUrl: './ditale-members.component.html',
   styleUrls: ['./ditale-members.component.css']
 })
-export class DitaleMembersComponent {
+export class DitaleMembersComponent  implements OnInit {
+
+  constructor(private route:ActivatedRoute){}
+  ngOnInit(): void {
+   console.log(this.route?.snapshot?.params['username'])
+  }
 
 }
--- Client/src/app/members/members-routing.module.ts
+++ Client/src/app/members/members-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeMembersComponent } from './home-members/home-members.component';
 import { ListMembersComponent } from './list-members/list-members.component';
+import { DitaleMembersComponent } from './ditale-members/ditale-members.component';
 
 const routes: Routes = [
   {path:"",component:HomeMembersComponent,children:
   [
-    { path :"",component:ListMembersComponent}
+    { path :"",component:ListMembersComponent},
+    { path :":username",component:DitaleMembersComponent}
   ]}
 ];
 
