13fc01988fa4d72e15078c6b7ce5fbd766ee746b
shahraieni <shahraieni.mehdi@gmail.com>
2023-12-17 22:25:18 +0330

    ngx

--- Client/src/app/members/ditale-members/ditale-members.component.ts
+++ Client/src/app/members/ditale-members/ditale-members.component.ts
@@ -1,7 +1,7 @@
 import { MemberService } from './../../_services/member.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { IMember } from 'src/app/_models/Member';
 
 @Component({
@@ -10,13 +10,41 @@ import { IMember } from 'src/app/_models/Member';
   styleUrls: ['./ditale-members.component.css']
 })
 export class DitaleMembersComponent  implements OnInit {
-  gulleryOption:NgxGalleryOptions;
-  gulleryImage:NgxGalleryImage;
-
   member:IMember;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
   constructor(private route:ActivatedRoute){}
   ngOnInit(): void {
-    this.member = this.route.snapshot.data['member'] as IMember
+    this.loademember();
+    this.loadOptions();
+  }
+  loademember(){
+    this.route.data.subscribe((data)=>{
+      this.member = data['member'] as IMember;
+      this.galleryImages = this.getimage();
+    })
+
+  }
+  private loadOptions(){
+     this.galleryOptions=[{
+       width:'300px',
+       height:'300px',
+       thumbnailsColumns:4,
+       imageAnimation:NgxGalleryAnimation.Slide,
+       preview:false
+     }]
+
+   }
+ private getimage(){
+    const images : NgxGalleryImage[]=[];
+    for(let image of this.member.photos){
+      images.push({
+        big:image.url,
+        small:image.url,
+        medium:image.url
+      })
+    }
+    return images
   }
 
 }
