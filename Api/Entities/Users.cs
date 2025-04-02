83a65923841313630e2875a48bf4b224b27f4bc0
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-04-02 21:42:42 +0330

    updateuserentity

--- Api/Entities/Users.cs
+++ Api/Entities/Users.cs
@@ -11,6 +11,7 @@ namespace Api.Entities
         [Key]
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
--- Api/obj/Debug/net5.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net5.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9261106baa40d897a900cd33edc6653d76d7cae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c39440b85da59306855d99e6c65eeeb7f1dd55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
