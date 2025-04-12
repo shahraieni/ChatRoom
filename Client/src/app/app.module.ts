c652844b691bdcdab06933c45bdbc9359e6fdf64
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-04-12 12:15:24 +0330

    shared

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,11 +6,11 @@ import {HttpClientModule} from "@angular/common/http";
 import { NavComponent } from './nav/nav.component'
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -26,13 +26,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right',
-      timeOut:5000,
-      progressBar:true,
-      progressAnimation:'increasing'
-    })
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Client/src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      timeOut:5000,
+      progressBar:true,
+      progressAnimation:'increasing'
+    })
+  ], 
+  exports:[
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
