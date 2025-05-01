38eeadd4aaff5526e4c84e5084df4ea9ee31a2af
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-05-01 10:23:10 +0330

    Add client

--- Api/Data/Migrations/20250501063747_ChengEntityUser.Designer.cs
+++ Api/Data/Migrations/20250501063747_ChengEntityUser.Designer.cs
@@ -3,14 +3,16 @@ using Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Api.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250501063747_ChengEntityUser")]
+    partial class ChengEntityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
