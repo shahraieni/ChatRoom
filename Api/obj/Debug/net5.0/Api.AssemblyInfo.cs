878722b7d4131aedc2a504204cfa3dc0447167c0
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-05-01 12:26:55 +0330

    Add SSL

--- Api/obj/Debug/net5.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net5.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2621f06fa301449e1b9aef6e6613aa9646a686")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5780ead1f1500ba858415f8e869691566892fb84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
