1bd63dce49868f24cdd9f811729cf55446e3a861
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-05-01 12:46:19 +0330

    dffdf

---
+++ Api/Data/Migrations/20250501091003_UpdadtUserPassword1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Data.Migrations
+{
+    public partial class UpdadtUserPassword1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "PasswordHash");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Password");
+        }
+    }
+}
--- Api/Entites/Users.cs
+++ Api/Entites/Users.cs
@@ -11,6 +11,7 @@ namespace Api.Entites
         [Key]
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
+        public byte[]  PasswordSalt { get; set; }
     }
 }
