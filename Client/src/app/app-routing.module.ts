d05716ae441911825243db8cedebd1b378ae232b
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-05-08 20:42:58 +0330

    notfoumd

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'members',  loadChildren: () => import('./members/members.module').then((x) => x.MembersModule) },
   { path: 'lists',loadChildren: () => import('./list/list.module').then((x) => x.ListModule) },
   { path: 'messages', loadChildren: () => import('./messages/messages.module').then((x) => x.MessagesModule) },
-  // { path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
