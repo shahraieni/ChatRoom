f75c99577209c2b47bcce0c547b58076b74c1fbc
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-05-09 07:26:26 +0330

    bootswatch

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -7,11 +7,11 @@ import {HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component'
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './shared/shared.module';
+
 
 
 
@@ -28,14 +28,9 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    BsDropdownModule.forRoot(),
     BrowserAnimationsModule,
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right',
-      timeOut:5000,
-      progressBar:true,
-      progressAnimation:'increasing'
-    })
+    SharedModule
+   
     
   ],
   providers: [],
