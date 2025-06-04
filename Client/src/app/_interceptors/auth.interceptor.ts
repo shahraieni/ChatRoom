7d0c6b5f45d8f42356869e2ab899d1ac058e5bc4
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-06-04 13:15:24 +0330

    .

--- Client/src/app/_interceptors/auth.interceptor.ts
+++ Client/src/app/_interceptors/auth.interceptor.ts
@@ -5,42 +5,40 @@ import {
   HttpEvent,
   HttpInterceptor,
   HttpHeaders,
-  HttpEventType
+  HttpEventType,
 } from '@angular/common/http';
-import { Observable, take, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
-import { User } from '../_model/account';
+import { take, tap, exhaustMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private accountService: AccountService) {}
 
-  currentUser : User;
-
-  constructor(private accountService : AccountService) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
-      this.currentUser = user;
-    });
-    if(this.currentUser){
-      request = request.clone({headers :new HttpHeaders().set(
-        'Authorization'
-        ,'Bearer ' + this.currentUser.token
-      ),
-      });
-    }
-    return next.handle(request).pipe(
-      tap((event)=>{
-        if(event.type === HttpEventType.Sent){
-               
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler ): Observable<HttpEvent<unknown>> {
+    return this.accountService.currentUser$.pipe(
+      take(1),
+      exhaustMap((user) => {
+        if (user) {
+          request = request.clone({
+            headers: new HttpHeaders().set(
+              'Authorization',
+              'Bearer ' + user?.token
+            ),
+          });
         }
-        if(event.type === HttpEventType.Response){
-          
-          const token = event.body?.userName;
-          if(token){
-            localStorage.setItem("user",JSON.stringify(event.body))
+        return next.handle(request);
+      }),
+      tap((event) => {
+        if (event.type === HttpEventType.Sent) {
+        }
+        if (event.type === HttpEventType.Response) {
+          const token = event?.body?.token;
+          if (token) {
+            localStorage.setItem('user', JSON.stringify(event.body));
           }
-          
         }
       })
     );
