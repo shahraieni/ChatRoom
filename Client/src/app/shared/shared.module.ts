dd8f5e8eb3be97b1e4dbc224146c0f4678ac88fa
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-06-16 09:00:12 +0330

    add PhotoEdit

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
 
 
 
@@ -23,14 +24,15 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
   TabsModule.forRoot(),
   NgxGalleryModule,
-
+  FileUploadModule
   ],
   exports :[
     BsDropdownModule,
     ToastrModule,
     TabsModule,
     NgxGalleryModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FileUploadModule
   ]
 })
 export class SharedModule { }
